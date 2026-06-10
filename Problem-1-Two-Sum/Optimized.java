84b574f91c5f0e87ff68c6051dd55d9f21bf3d90
Rahul Singh <159793792+rahul-sisodiya@users.noreply.github.com>
2026-06-10 16:17:41 +0530

    Implement optimized two-sum solution in Java

---
+++ Problem-1-Two-Sum/Optimized.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+
+            // Check if complement already exists
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            // Store current number with index
+            map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+}
